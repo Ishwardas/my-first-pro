127d42df1802024ce51e52ea01b94b6d84e857a7
Ihwardas <ishwar39@gmail.com>
2024-10-22 01:59:16 +0530

    my first copy of project

---
+++ src/main/java/com/registration8/Registration8Application.java
@@ -0,0 +1,13 @@
+package com.registration8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Registration8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Registration8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/registration8/Registration8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Registration8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
